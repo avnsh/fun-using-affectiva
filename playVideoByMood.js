3ea679e3e8983570a27b95198fb3fc9981f50589
avnsh <avnsh.ka@gmail.com>
2019-07-16 00:28:45 +0530

    Create playVideoByMood.js

---
+++ playVideoByMood.js
@@ -0,0 +1,29 @@
+const moodToQueryMap = {
+    anger: '',
+    disgust: '',
+    fear: 'hanuman chalisa',
+    joy: 'uptown funk',
+    sadness: 'my heart will go on',
+    surprise: '',
+};
+const apiKey = "AIzaSyDBPzjEDZK_ZGOZcYQT7lm0VTdzjp5w95w";
+const content = document.getElementById('content');
+const currentEmotion = document.getElementById('currentEmotion');
+
+function playVideo(mood) {
+    currentEmotion.innerHTML = mood;
+    makeApiCall(moodToQueryMap[mood]).then(function (videoId) {
+        // set content with video id
+        content.innerHTML = `
+        <iframe id="video" width="420" height="315" src="https://www.youtube.com/embed/${videoId}?autoplay=1"></iframe>
+        `
+    });
+}
+
+function makeApiCall(query) {
+    return fetch(`https://www.googleapis.com/youtube/v3/search?part=id&q=${query}&key=${apiKey}`)
+    .then(res => res.json())
+    .then(function(resp) {
+      return resp.items[0].id.videoId;
+    }).catch(err => console.log(err));
+}
